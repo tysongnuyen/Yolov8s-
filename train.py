b0b3e519ddc2519e323981a8068f3132dd7371dd
UltralyticsAssistant <web@ultralytics.com>
2026-07-06 04:13:15 +0000

    Auto-format by https://ultralytics.com/actions

--- train.py
+++ train.py
@@ -1,6 +1,16 @@
-import ultralytics
 from ultralytics import YOLO
+
 model = YOLO("yolov8s.pt")
-model.export(imgsz = 360,opset=12)
+model.export(imgsz=360, opset=12)
 if __name__ == "__main__":
-    model.train(data="data.yaml", epochs=2, imgsz=480, batch=12, device='cuda:0', workers=8, project="gold_ball/train", name="yolov8n_golf_ball",save=True)
+    model.train(
+        data="data.yaml",
+        epochs=2,
+        imgsz=480,
+        batch=12,
+        device="cuda:0",
+        workers=8,
+        project="gold_ball/train",
+        name="yolov8n_golf_ball",
+        save=True,
+    )
